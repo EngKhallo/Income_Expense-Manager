66a3d889cfc4051431c50b7823231de5d24b849b
EngKhallo <engkhalid.ict@gmail.com>
2022-08-10 01:59:10 +0300

    sidebar

---
+++ tools/sidebar.php
@@ -0,0 +1,66 @@
+<div class="nk-sidebar">
+            <div class="nk-nav-scroll">
+                <ul class="metismenu" id="menu">
+                    <li class="nav-label text-primary">Dashboard</li>
+                    <li>
+                        <a class="#" href="./index.php" aria-expanded="false">
+                            <i class="fas fa-tachometer-alt"></i><span class="nav-text">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="" href="transactions.php" aria-expanded="false">
+                            <i class="fas fa-money-check-alt"></i><span class="nav-text">Transactions</span>
+                        </a>
+                    </li>
+                    <li class="mega-menu mega-menu-sm">
+                        <a class="" href="./incomes.php" aria-expanded="false">
+                            <i class="fas fa-sort-amount-up-alt"></i><span class="nav-text">Incomes</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="" href="./expenses.php" aria-expanded="false">
+                            <i class="fas fa-sort-amount-down-alt"></i><span class="nav-text">Expenses</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="" href="javascript:void()" aria-expanded="false">
+                            <i class="fas fa-window-restore"></i> <span class="nav-text">Accounts</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="" href="javascript:void()" aria-expanded="false">
+                            <i class="fas fa-th-large"></i><span class="nav-text">Budget</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="fas fa-cogs"></i><span class="nav-text">Settings</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="./Income_category.php">Income Category</a></li>
+                            <li><a href="./expense_category.php">Expense Category</a></li>
+                        </ul>
+                    </li>
+
+
+
+                    <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="far fa-chart-bar"></i><span class="nav-text">Reports</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="#"><i class="fas fa-project-diagram"></i>Income vs Expenses</a></li>
+                            <li><a href="#"><i class="fas fa-chart-line"></i> Data in Graphs</a></li>
+                            <li><a href="#"><i class="fas fa-plus-square"></i> Income Reports</a></li>
+                            <li><a href="#"><i class="fas fa-minus-square"></i> Expense Reports</a></li>
+                            <li><a href="#"><i class="fas fa-chart-area"></i> OverAll Data</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="" href="javascript:void()" aria-expanded="false">
+                            <i class="fas fa-users-cog"></i><span class="nav-text">Users</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
