f053419049b24782fbe0461ee5456387163c4650
EngKhallo <engkhalid.ict@gmail.com>
2022-08-10 01:52:42 +0300

    changed db name

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,6 @@
 <?php
      function connect(){
-        $conn = mysqli_connect($servername="localhost", $username="root", $password="", $dbname="daily_income_expense_manager");
+        $conn = mysqli_connect($servername="localhost", $username="root", $password="", $dbname="income_expense manager");
         if(!$conn) die("connection failed");
         return $conn;
     }
